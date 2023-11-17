4ee73a81ae2a8ebdb158455443dce5289e9fb6f9
evelyn <evelyn.ribeiro0807@gmail.com>
2023-11-17 11:41:59 -0300

    JavaScript

---
+++ React/eventplus/src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,65 @@
+import React, { useEffect, useState } from "react";
+import "./HomePage.css";
+
+import Banner from "../../components/Banner/Banner";
+import MainContent from "../../components/MainContent/MainContent";
+import VisionSection from "../../components/VisionSection/VisionSection";
+import ContactSection from "../../components/ContactSection/ContactSection";
+import Title from "../../components/Title/Title";
+import NextEvent from "../../components/NextEvent/NextEvent";
+import Container from "../../components/Container/Container";
+import api from "../../Services/Service";
+
+import { nextEventResource } from "../../Services/Service";
+const HomePage = () => {
+  const [nextEvents, setNextEvents] = useState([]);
+ 
+
+  // roda somente na inicialização do componente
+  useEffect(() => {
+    async function getNextEvents() {
+      try {
+        const promise = await api.get(nextEventResource);
+        const dados = await promise.data;
+        console.log(dados);
+        setNextEvents(dados); //atualiza o state
+      } catch (error) {
+        console.log("Deu ruim na api!");
+      }
+    }
+
+    getNextEvents(); //chama a função
+  }, []);
+
+  return (
+    <MainContent>
+      <Banner />
+
+      {/* PRÓXIMOS EVENTOS */}
+      <section className="proximos-eventos">
+        <Container>
+          <Title titleText={"Próximos Eventos"} />
+
+          <div className="events-box">
+            {nextEvents.map((e) => {
+              return (
+                <NextEvent
+                  key={e.idEvento}
+                  title={e.nomeEvento}
+                  description={e.descricao}
+                  eventDate={e.dataEvento}
+                  idEvent={e.idEvento}
+                />
+              );
+            })}
+          </div>
+        </Container>
+      </section>
+
+      <VisionSection />
+      <ContactSection />
+    </MainContent>
+  );
+};
+
+export default HomePage;
